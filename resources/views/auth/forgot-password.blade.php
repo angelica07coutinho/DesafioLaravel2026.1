c559f2de21e65e74da660e8371ffd5af887381a9
Angélica <angelica07coutinho@gmail.com>
2026-02-05 15:10:07 -0300

    fix: atualiza redefinir senha

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -1,6 +1,6 @@
 <x-guest-layout>
-    <div class="mb-4 text-sm text-gray-600 dark:text-gray-400">
-        {{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}
+    <div class="mb-4 text-sm text-[#a066a6]">
+        {{ __('Informe seu email cadastrado para receber um link de redefinição de senha.') }}
     </div>
 
     <!-- Session Status -->
@@ -16,9 +16,9 @@
             <x-input-error :messages="$errors->get('email')" class="mt-2" />
         </div>
 
-        <div class="flex items-center justify-end mt-4">
+        <div class="flex items-center justify-center mt-4">
             <x-primary-button>
-                {{ __('Email Password Reset Link') }}
+                {{ __('Enviar Link') }}
             </x-primary-button>
         </div>
     </form>
