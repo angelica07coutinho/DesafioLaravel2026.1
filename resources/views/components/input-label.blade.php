47922a22501a0149960a00790b3965d88bc4b0dd
Angélica <angelica07coutinho@gmail.com>
2026-02-08 15:53:23 -0300

    fix: atualiza cor de textos

--- resources/views/components/input-label.blade.php
+++ resources/views/components/input-label.blade.php
@@ -1,5 +1,5 @@
 @props(['value'])
 
-<label {{ $attributes->merge(['class' => 'block font-medium text-sm text-[#7066a6]']) }}>
+<label {{ $attributes->merge(['class' => 'block font-medium text-sm text-[#4a0051]']) }}>
     {{ $value ?? $slot }}
 </label>
--- resources/views/components/primary-button.blade.php
+++ resources/views/components/primary-button.blade.php
@@ -1,3 +1,3 @@
-<button {{ $attributes->merge(['type' => 'submit', 'class' => 'inline-flex items-center px-4 py-2 bg-[#f8e9f9] border border-transparent hover:border-[#7066a6] rounded-md font-bold text-base text-[#7066a6] uppercase tracking-widest focus:bg-[#7066a6] focus:text-[#f8e9f9] focus:outline-none focus:ring-2 focus:ring-black focus:ring-offset-2 transition ease-in-out duration-150']) }}>
+<button {{ $attributes->merge(['type' => 'submit', 'class' => 'inline-flex items-center px-4 py-2 bg-[#f8e9f9] border border-transparent hover:border-[#7066a6] rounded-md font-bold text-base text-[#4a0051] uppercase tracking-widest focus:bg-[#7066a6] focus:text-[#f8e9f9] focus:outline-none focus:ring-2 focus:ring-black focus:ring-offset-2 transition ease-in-out duration-150']) }}>
     {{ $slot }}
 </button>
