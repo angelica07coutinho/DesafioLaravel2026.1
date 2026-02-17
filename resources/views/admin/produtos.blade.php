2fbb7bc851e55d3b8cbe16973955f956f94634c0
Angélica <angelica07coutinho@gmail.com>
2026-02-17 20:31:45 -0300

    feat: adiciona pagina de gerenciamento de produtos para admin

---
+++ resources/views/admin/produtos.blade.php
@@ -0,0 +1,51 @@
+<x-app-layout>
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 flex flex-col gap-4">
+            @if (session('success'))
+            <div class="text-green-600 text-center">
+                {{ session('success') }}
+            </div>
+            @endif
+            @if ($errors->any())
+            <div class="text-red-600 text-center">
+                <ul>
+                    @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+            </div>
+            @endif
+            <div class="w-full gap-4 flex">
+                <x-filtro-categoria route='admin.produtos.index'></x-filtro-categoria>
+                <x-pesquisa-produto route='admin.produtos.index'></x-pesquisa-produto>
+            </div>
+            <div class="overflow-auto sm:rounded-lg border-2 border-[#a066a6]">
+                <table class="min-w-full">
+                    <thead class="bg-[#a066a6] text-[#f8e9f9]">
+                        <tr>
+                            <th class="px-3 py-2 text-center text-lg font-bold text-[#4a0051] border-r border-[#4a0051] w-5">ID</th>
+                            <th class="px-3 py-2 text-center text-lg font-bold text-[#4a0051] border-r border-[#4a0051]">Produto</th>
+                            <th class="px-3 py-2 text-center text-lg font-bold text-[#4a0051] border-r border-[#4a0051]">Vendedor</th>
+                            <th class="px-3 py-2 text-center text-lg font-bold text-[#4a0051] border-r border-[#4a0051] w-20">Preço</th>
+                            <th class="px-3 py-2 text-center text-lg font-bold text-[#4a0051] w-20">Ações</th>
+                        </tr>
+                    </thead>
+                    <tbody class="bg-[#4a0051c6] divide-y-2 divide-[#a066a6] text-[#f8e9f9]">
+                        @foreach ($produtos as $produto)
+                        <tr>
+                            <td class="px-3 py-2 whitespace-nowrap border-r border-[#a066a6] text-center">{{ $produto->id }}</td>
+                            <td class="px-3 py-2 whitespace-nowrap border-r border-[#a066a6]">{{ $produto->nome }}</td>
+                            <td class="px-3 py-2 whitespace-nowrap border-r border-[#a066a6]">{{ $produto->vendedor->name }}</td>
+                            <td class="px-3 py-2 whitespace-nowrap border-r border-[#a066a6]">R${{ $produto->preco }}</td>
+                            <td class="px-3 whitespace-nowrap">
+                                <a href="#" class="text-xl hover:text-[#a066a6]" data-bs-toggle="modal" data-bs-target="#viewProdutoModal{{ $produto->id }}"><i class="bi bi-eye-fill"></i></a>
+                                <a href="#" class="text-xl ms-3 hover:text-[#a066a6]" data-bs-toggle="modal" data-bs-target="#deleteProdutoModal{{ $produto->id }}"><i class="bi bi-trash-fill"></i></a>
+                            </td>
+                        </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
+        </div> 
+    </div>
+</x-app-layout>
