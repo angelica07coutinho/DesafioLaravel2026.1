74d9c5c520afd1a8254181338753da8f6edc3ba1
Angélica <angelica07coutinho@gmail.com>
2026-02-19 10:19:15 -0300

    feat: adiciona o vendedor na visualizacao pelo admin e corrige ids

--- resources/views/admin/view-produto.blade.php
+++ resources/views/admin/view-produto.blade.php
@@ -5,30 +5,30 @@
             <div class="flex gap-4 w-full">
                 <div class="w-full">
                     <x-input-label :value="__('Nome do Produto *')" />
-                    <x-text-input id="create-nome-produto" class="block mt-1 w-full" type="text" name="nome" value="{{ $produto->nome }}" readonly />
+                    <x-text-input id="view-nome-produto" class="block mt-1 w-full" type="text" name="nome" value="{{ $produto->nome }}" readonly />
                     <x-input-error :messages="$errors->get('nome')" class="mt-2" />
                 </div>                
             </div>
             <div class="flex gap-4">
                 <div>
                     <x-input-label class="text-nowrap" :value="__('Foto do Produto')" />
-                    <x-text-input id="create-foto" class="block mt-1 h-48 w-48" type="file" name="foto_produto" value="{{ $produto->foto_produto }}" readonly/>
+                    <x-text-input id="view-foto" class="block mt-1 h-48 w-48" type="file" name="foto_produto" value="{{ $produto->foto_produto }}" readonly/>
                     <x-input-error :messages="$errors->get('foto_produto')" class="mt-2" />
                 </div>
                 <div class="flex flex-col gap-3 w-full">
                     <div>
                         <x-input-label :value="__('Categoria *')" />
-                        <x-text-input id="create-categoria" class="block mt-1 w-full" type="text" name="categoria" value="{{ $produto->categoria->nome }}" readonly/>
+                        <x-text-input id="view-categoria" class="block mt-1 w-full" type="text" name="categoria" value="{{ $produto->categoria->nome }}" readonly/>
                         <x-input-error :messages="$errors->get('id_categoria')" class="mt-2" />
                     </div>
                     <div>
                         <x-input-label :value="__('Preço (R$)*')" />
-                        <x-text-input id="create-preco" class="block mt-1 w-full" type="number" name="preco" value="{{ $produto->preco }}" readonly />
+                        <x-text-input id="view-preco" class="block mt-1 w-full" type="number" name="preco" value="{{ $produto->preco }}" readonly />
                         <x-input-error :messages="$errors->get('preco')" class="mt-2" />
                     </div>
                     <div class="w-full">
                         <x-input-label :value="__('Estoque *')" />
-                        <x-text-input id="create-quantidade" class="block mt-1 w-full" type="number" name="quantidade" value="{{ $produto->quantidade }}" readonly />
+                        <x-text-input id="view-quantidade" class="block mt-1 w-full" type="number" name="quantidade" value="{{ $produto->quantidade }}" readonly />
                         <x-input-error :messages="$errors->get('quantidade')" class="mt-2" />
                     </div>
                 </div>
@@ -36,9 +36,22 @@
             <div class="flex gap-4 w-full">
                 <div class="w-full">
                     <x-input-label :value="__('Descrição *')" />
-                    <x-text-input id="create-descricao" class="block mt-1 w-full" type="text" name="descricao" value="{{ $produto->descricao }}" readonly />
+                    <x-text-input id="view-descricao" class="block mt-1 w-full" type="text" name="descricao" value="{{ $produto->descricao }}" readonly />
                     <x-input-error :messages="$errors->get('descricao')" class="mt-2" />
                 </div>                
+            </div>
+            <h2 class="text-lg font-bold text-[#4a0051] mt-2">Informações do Vendedor</h2>
+            <div class="flex gap-4 w-full">
+                <div class="w-full">
+                    <x-input-label :value="__('Nome *')" />
+                    <x-text-input id="view-vendedor" class="block mt-1 w-full" type="text" name="vendedor" value="{{ $produto->vendedor->name }}" readonly />
+                    <x-input-error :messages="$errors->get('vendedor')" class="mt-2" />
+                </div>
+                <div class="w-full">
+                    <x-input-label :value="__('Email *')" />
+                    <x-text-input id="view-email-vendedor" class="block mt-1 w-full" type="text" name="email_vendedor" value="{{ $produto->vendedor->email }}" readonly />
+                    <x-input-error :messages="$errors->get('email_vendedor')" class="mt-2" />
+                </div>
             </div>
         </div>
         <div class="flex items-center justify-end mt-4 gap-2">
