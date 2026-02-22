0c731e01a70d95e5dade97573f8b9524f1e38235
Angélica <angelica07coutinho@gmail.com>
2026-02-22 20:58:31 -0300

    fix: ajusta estilos da navbar

--- resources/views/components/nav-link.blade.php
+++ resources/views/components/nav-link.blade.php
@@ -2,8 +2,8 @@
 
 @php
 $classes = ($active ?? false)
-            ? 'inline-flex items-center px-1 pt-1 border-b-2 border-[#a066a6] text-sm font-medium leading-5 text-gray-100 focus:outline-none focus:border-[#a066a6] transition duration-150 ease-in-out'
-            : 'inline-flex items-center px-1 pt-1 border-b-2 border-transparent text-sm font-medium leading-5 text-[#f8e9f9] hover:text-[#a066a6] hover:border-[#a066a6] focus:outline-none focus:text-[#f8e9f9] focus:border-[#f8e9f9] transition duration-150 ease-in-out';
+            ? 'inline-flex items-center px-1 pt-1 border-b-[3px] border-[#a066a6] text-md font-medium leading-5 text-gray-100 focus:outline-none focus:border-[#a066a6] transition duration-150 ease-in-out'
+            : 'inline-flex items-center px-1 pt-1 border-b-[3px] border-transparent text-md font-medium leading-5 text-[#f8e9f9] hover:text-[#a066a6] hover:border-[#a066a6] focus:outline-none focus:text-[#f8e9f9] focus:border-[#f8e9f9] transition duration-150 ease-in-out';
 @endphp
 
 <a {{ $attributes->merge(['class' => $classes]) }}>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -22,15 +22,6 @@
         <div class="bg-black">
             <div class="min-h-screen bg-[url('/public/images/hexagonos_roxo_escuro.png')] bg-repeat bg-[length:540px_590px]">
                 @include('layouts.navigation')
-                <!-- Page Heading -->
-                <!-- @isset($header)
-                    <header class="bg-white dark:bg-gray-800 shadow">
-                        <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                            {{ $header }}
-                        </div>
-                    </header>
-                @endisset -->
-                <!-- Page Content -->
                 <main>
                     {{ $slot }}
                 </main>
