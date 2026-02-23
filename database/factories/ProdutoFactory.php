f68de4f2c61b539fd2d54220bb2302e4029f3f8d
Angélica <angelica07coutinho@gmail.com>
2026-02-22 23:16:45 -0300

    feat: adiciona imagem padrao de produtos e ajusta na factory

--- database/factories/ProdutoFactory.php
+++ database/factories/ProdutoFactory.php
@@ -5,6 +5,8 @@ namespace Database\Factories;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use App\Models\Produto;
 use App\Models\User;
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Storage;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Produto>
@@ -18,10 +20,18 @@ class ProdutoFactory extends Factory
      */
     public function definition(): array
     {
+        $imagemDefault = 'produtos/default.png';
+        if (!Storage::disk('public')->exists($imagemDefault)) {
+            $sourcePath = public_path('images/default.png');
+            if (File::exists($sourcePath)) {
+                Storage::disk('public')->put($imagemDefault, File::get($sourcePath));
+            }
+        }
+        
         return [
-            'nome' => fake()->word(),
-            'descricao' => fake()->sentence(),
-            'foto_produto' => fake()->imageUrl(),
+            'nome' => fake()->sentence(3),
+            'descricao' => fake()->paragraph(8),
+            'foto_produto' => $imagemDefault,
             'preco' => fake()->randomFloat(2, 1, 100),
             'quantidade' => fake()->numberBetween(1, 100),
             'id_vendedor' => User::factory(),
